e9471879a695ec067e6b5f69545c7665721b0011
eleagan <eamon.laughton-eagan@hc-sc.gc.ca>
2023-08-04 17:11:29 -0400

    created deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+echo "Switching to branch deploy"
+git checkout deploy
+echo "Building app..."
+npm run build
+
+echo "Deploying files to server..."
+scp -r build/* eamon@172.105.99.42:/var/www/172.105.99.42/
+
+echo "Done!"
