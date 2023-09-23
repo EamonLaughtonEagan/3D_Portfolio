337079b4459234edbe0ee857706968bc50474e34
Eamon <eamonlaughtoneagan@gmail.com>
2023-09-23 01:29:58 -0700

    tinkering

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,11 +9,11 @@ const Hero = () => {
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl 
       mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+          <div className='w-5 h-5 rounded-full bg-[#D82651]' />
+          <div className='w-1 sm:h-80 h-40 red-gradient'/>
         </div>
         <div className='pb-[200px]'>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Eamon</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#D82651]'>Eamon</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop web applications, <br className='sm:block-hidden'/> and user interfaces
           </p>
